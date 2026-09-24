8829e0b31592324d6a18370c43881794f91e1bc8
Sonam Verma <harshvp967@gmail.com>
2026-09-24 19:43:19 +0530

    Fix obsolete agent command

--- apps/commands/src/command.module.ts
+++ apps/commands/src/command.module.ts
@@ -3,13 +3,11 @@ import { CommandModule as ExternalCommandModule } from 'nestjs-command';
 import { DatabaseModule } from '@gitroom/nestjs-libraries/database/prisma/database.module';
 import { RefreshTokens } from './tasks/refresh.tokens';
 import { ConfigurationTask } from './tasks/configuration';
-import { AgentRun } from './tasks/agent.run';
-import { AgentModule } from '@gitroom/nestjs-libraries/agent/agent.module';
 
 @Module({
-  imports: [ExternalCommandModule, DatabaseModule, AgentModule],
+  imports: [ExternalCommandModule, DatabaseModule],
   controllers: [],
-  providers: [RefreshTokens, ConfigurationTask, AgentRun],
+  providers: [RefreshTokens, ConfigurationTask],
   get exports() {
     return [...this.imports, ...this.providers];
   },
