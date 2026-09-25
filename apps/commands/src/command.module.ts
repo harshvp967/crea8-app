ad1ed904f4a44e97a70b898fd1c3ec70e2876266
Sonam Verma <harshvp967@gmail.com>
2026-09-25 12:07:53 +0530

    Fix command Temporal module wiring

--- apps/commands/src/command.module.ts
+++ apps/commands/src/command.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CommandModule as ExternalCommandModule } from 'nestjs-command';
 import { DatabaseModule } from '@gitroom/nestjs-libraries/database/prisma/database.module';
+import { getTemporalModule } from '@gitroom/nestjs-libraries/temporal/temporal.module';
 import { RefreshTokens } from './tasks/refresh.tokens';
 import { ConfigurationTask } from './tasks/configuration';
 
 @Module({
-  imports: [ExternalCommandModule, DatabaseModule],
+  imports: [ExternalCommandModule, DatabaseModule, getTemporalModule(false)],
   controllers: [],
   providers: [RefreshTokens, ConfigurationTask],
   get exports() {
